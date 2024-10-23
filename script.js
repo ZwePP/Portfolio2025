80dd6cb5b530c6b3f10341d4fefc479659bca3da
Zwe Pyi Phyo <99266799+ZwePP@users.noreply.github.com>
2024-10-23 22:31:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleDropdown() {
+      var content = document.getElementById("dropdownContent");
+      content.classList.toggle("show");
+    }
+
+function toggleDropdown2() {
+        var content = document.getElementById("dropdownContent2");
+        content.classList.toggle("show");
+    }
+
+function toggleDropdown3() {
+        var content = document.getElementById("dropdownContent3");
+        content.classList.toggle("show");
+    }
+
+function toggleDropdown4() {
+        var content = document.getElementById("dropdownContent4");
+        content.classList.toggle("show");
+    }
+
+function toggleDropdown5() {
+        var content = document.getElementById("dropdownContent5");
+        content.classList.toggle("show");
+    }
